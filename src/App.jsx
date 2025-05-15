034aae28397581cd49b6693f64c2aca9b1bde45c
Himanshu <hsjethi1011@gmail.com>
2025-05-15 19:08:20 +0530

    meme-generator project done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import HeaderComponent from "./components/HeaderComponent"
+import Main from "./components/Main"
+
+export default function App() {
+    
+    return (
+        <>
+            <HeaderComponent />
+            <Main />
+        </>
+    )
+}
